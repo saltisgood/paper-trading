4bca50a96c30330d445334dbf879b692926b17c4
saltisgood <saltisgood@users.noreply.github.com>
2021-11-27 05:29:13 +0000

    ✅ Ready to clone and code.

--- paper_trading/base.py
+++ paper_trading/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+paper_trading base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the paper_trading project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
